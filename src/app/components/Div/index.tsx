6906ae97137b7faaa25799e7784cf7db9bb91ccf
westhyun <pkwesst@gmail.com>
2023-06-24 11:11:12 +0900

    chore: 🚚 Revised file name(Div -> FlexBox)

--- src/app/components/Div/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Div = styled.div<{
-  display?: string;
-  fontSize?: string;
-}>`
-  display: ${props => props.display || 'inherit'};
-  font-size: ${props => props.fontSize || 'inherit'};
-`;
